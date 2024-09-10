09a6365556b90686d513331e02c6b3894eeacf67
Margarita Potylitsyna <148562840+Ritumka@users.noreply.github.com>
2024-09-10 21:53:25 +0000

    100%

--- app.rb
+++ app.rb
@@ -20,6 +20,7 @@ get("/wizard_add") do
   @second_num = params.fetch("second_num").to_f
 
   @result = @first_num + @second_num
+  
   erb(:add_result)
 end
 
@@ -37,10 +38,15 @@ get("/wizard_subtract") do
 end
 
 get("/multiply") do
-  erb(:multiplication_form)
+  erb(:mult_form)
 end
 
-get("/wizard multiply") do
+get("/wizard_multiply") do
+  @first_num = params.fetch("first_num").to_f
+  @second_num = params.fetch("second_num").to_f
+
+  @result = @first_num * @second_num
+
   erb(:mult_result)
 end
 
@@ -49,5 +55,10 @@ get("/divide") do
 end
 
 get("/wizard_divide") do
+  @first_num = params.fetch("first_num").to_f
+  @second_num = params.fetch("second_num").to_f
+
+  @result = @first_num / @second_num
+
   erb(:div_result)
 end
